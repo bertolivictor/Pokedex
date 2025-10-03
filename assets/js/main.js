48d10603b73919d4bb66b3752f7fdcde00a5be01
Victor Bertoli <bertoli.victor91@gmai.com>
2025-10-03 17:02:23 -0300

    finalizar estilização e iniciar js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log('Sucesso');
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch (url)
+    .then (function (response) {
+        return response.json();
+    })
+    .then (function (response) {
+        console.log (response);
+    })
+    .catch (function (error) {
+        console.error(error);
+    })
+    .finally (function () {
+        console.log ('Requisição concluída');
+    })

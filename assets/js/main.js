8d892de3abf89385dc1eb952313e77fd37c93ed1
Victor Bertoli <bertoli.victor91@gmail.com>
2025-10-13 21:14:33 -0300

    continuar desenvolvimento da lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,20 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 function convertPokemonToLi(pokemon) {
     return `
         <li class="card">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('') }
+                    
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
             </div>            
         </li>  
     `
@@ -21,6 +24,6 @@ const pokemonOl = document.getElementById("pokemonOl")
 
 pokeApi.getPokemons().then((pokemonsList = []) => {
 
-    pokemonOl.innerHTML += pokemonsList.map(convertPokemonToLi).join('')
+    pokemonOl.innerHTML = pokemonsList.map(convertPokemonToLi).join('')
 
 })

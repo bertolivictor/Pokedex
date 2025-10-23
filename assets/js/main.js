f91d338d12765df5076406b6fee4d4e22487c721
Victor Bertoli <bertoli.victor91@gmai.com>
2025-10-23 13:30:58 -0300

    melhorar ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let offset = 0;
-const limit = 10;
+const limit = 12;
 const maxRecords = 151
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
@@ -11,17 +11,20 @@ const loadMoreButton = document.getElementById("load-btn")
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="card ${pokemon.type}">
-            <span class="number">#${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    
-                </ol>
-                <img src="${pokemon.image}" alt="${pokemon.name}">
-            </div>            
-        </li>  
+        <a href="#" class="details-link">
+            <li class="card ${pokemon.type}">
+                
+                <span class="number">#${pokemon.order}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        
+                    </ol>
+                    <img src="${pokemon.image}" alt="${pokemon.name}">
+                </div>           
+            </li>
+        </a>  
     `
 }
 function loadPokemonItens(offset, limit) {
